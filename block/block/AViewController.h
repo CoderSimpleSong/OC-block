370ff59ec9d76631879e238d446d08dabed964b9
sj <83694281@qq.com>
2018-11-28 14:52:11 +0800

    qqq

---
+++ block/block/AViewController.h
@@ -0,0 +1,13 @@
+//
+//  AViewController.h
+//  block
+//
+//  Created by 创联 on 2018/11/28.
+//  Copyright © 2018年 宋佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AViewController : UIViewController
+
+@end
--- block/block/CViewController.h
+++ block/block/CViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CViewController.h
 //  block
 //
 //  Created by 创联 on 2018/11/28.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CViewController : UIViewController
 
 @end
-
